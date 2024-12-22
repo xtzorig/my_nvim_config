12c7e002e584c153a464486455440556f6896ae4
xtzorig <xtzorig@gmail.com>
2024-12-22 12:19:30 +0300

    update git_commands

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -65,8 +65,8 @@ vim.opt.relativenumber = false
 
 
 --Search settings
--- vim.opt.ignorecase = true -- ignore case when searching
--- vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 
 --Cursor line

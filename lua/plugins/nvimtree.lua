45546274dbb0e0ceefc8804f40c2d7fb7e9a7414
xtzorig <xtzorig@gmail.com>
2024-12-30 19:19:41 +0300

    everything OK, befor trying to modify toggleterm horizontaly

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -7,16 +7,46 @@ return {
   },
   config = function()
     require("nvim-tree").setup {
-      --update_cwd = true, -- Automaically update the current working directory
+      update_cwd = true, -- Automaically update the current working directory
 
       update_focused_file = {
                 enable = true, --Enable updating focused file
-                update_cwd = true, --Update cwd when opening a file
+                -- update_cwd = true, --Update cwd when opening a file
       },
       view = {
         width = 35,
         relativenumber = false,
         -- number = false,
+      },
+      -- change folder arrow icons
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
+            },
+          },
+        },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = {
+        ignore = false,
       },
     }
 
